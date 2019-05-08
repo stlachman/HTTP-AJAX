0458d70dfd74503afe517c356145eeb2b5469110
Seth Lachman <lachmanseth17@gmail.com>
2019-05-08 15:17:50 -0400

    Converted app component to class component and imported axios library. Created fetch functionality and set up simple view to show list of friends.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h2>Hello</h2>
-    </div>
-  );
+class App extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			friends: []
+		};
+	}
+
+	componentDidMount() {
+		axios
+			.get(`http://localhost:5000/friends`)
+			.then((res) => this.setState({ friends: res.data }))
+			.catch((err) => console.log(err));
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Hello</h2>
+        {this.state.friends.map(friend => <p key={friend.id}>{friend.name}</p>)}
+			</div>
+		);
+	}
 }
 
 export default App;
