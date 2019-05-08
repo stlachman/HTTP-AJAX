7a6c5a0b3aded80fa83be6c08b1d0b747d1632f4
Seth Lachman <lachmanseth17@gmail.com>
2019-05-08 19:02:00 -0400

    Added styles to navigation menu.

--- friends/src/components/Navigation.js
+++ friends/src/components/Navigation.js
@@ -1,15 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
-const Navigation = prop => {
+const NavBar = styled.nav`
+  background: #eee;
+  width: 100%;
+`;
+
+const NavContainer = styled.ul`
+  margin: 0 0 0 3rem;
+  padding: 1.5rem 0;
+  width: 100%;
+`;
+
+const NavList = styled.li`
+  display: inline-block;
+  margin: 0 2rem;
+
+  &:first-of-type {
+    margin-left: 0;
+  }
+`;
+
+const NavElement = styled(NavLink)`
+  text-decoration: none;
+  color: #000;
+  font-size: 1.5rem;
+`;
+
+const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/add-friend">Add Friend</NavLink></li>
-        <li><NavLink to="/update-friend">Update Friend</NavLink></li>
-      </ul>
-    </nav>
+    <NavBar>
+      <NavContainer>
+        <NavList><NavElement to="/">Home</NavElement></NavList>
+        <NavList><NavElement to="/add-friend">Add Friend</NavElement></NavList>
+        <NavList><NavElement to="/update-friend">Update Friend</NavElement></NavList>
+      </NavContainer>
+    </NavBar>
   )
 }
 
