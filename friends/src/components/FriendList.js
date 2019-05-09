2affc102de21259301b9e22dd3ea6f05ad87df96
Seth Lachman <lachmanseth17@gmail.com>
2019-05-09 18:20:03 -0400

    Implemented more styles.

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -8,33 +8,58 @@ const Container = styled.div`
 	text-align: center;
 `;
 
-const Title = styled.h1`font-size: 3rem;`;
+const Title = styled.h1`font-size: 3.5rem;`;
 const SubTitle = styled.h2`
 	font-size: 1.6rem;
-	transition: 0.2s opacity ease-in-out;
-	&:hover {
-		cursor: pointer;
-		opacity: 0.6;
-	}
 `;
 
+const FriendItem = styled.div`
+  background: #F5F5F5;
+  max-width: 400px;
+  margin: 1.5rem auto 0;
+  padding: 2rem 1rem;
+`;
+
+const Button = styled.button`
+  padding: 1rem 1.6rem;
+  border: 0;
+	border-radius: 0.5rem;
+	background: #eee;
+	font-size: 1.4rem;
+	font-weight: bold;
+	box-shadow: 3px 2px 2px 0px rgba(0, 0, 0, 0.3);
+  margin: 1.5rem 2rem 0;
+  transition: 0.2s all ease-in;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+`;
+
+const DeleteButton = styled(Button)`
+  background: #F6546A;
+`;
+
+
 const FriendList = (props) => {
-	function routeToItem(ev, friend) {
-		ev.preventDefault();
+	function routeToItem(event, friend) {
+		event.preventDefault();
 		props.history.push(`/friends/${friend.id}`);
 	}
 	return (
 		<Container>
 			<Title>Friends List</Title>
 			{props.friends.map((friend) => (
-				<div key={friend.id}>
-					<SubTitle onClick={(ev) => routeToItem(ev, friend)}>Name: {friend.name}</SubTitle>
+				<FriendItem key={friend.id}>
+					<SubTitle>Name: {friend.name}</SubTitle>
 					<p>Age: {friend.age}</p>
 					<p>
 						Email: <a href={`mailto:${friend.email}`}>{friend.email}</a>
 					</p>
-					<button onClick={() => props.deleteFriend(friend.id)}>Delete Friend</button>
-				</div>
+          <Button onClick={(event) => routeToItem(event, friend)}>View Friend</Button>
+					<DeleteButton onClick={() => props.deleteFriend(friend.id)}>Delete Friend</DeleteButton>
+				</FriendItem>
 			))}
 		</Container>
 	);
